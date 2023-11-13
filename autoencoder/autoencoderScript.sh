2a8bb128c0fb12b29d3f417ded7efd1b711e9cde
Hayden McAlister <mcaha814@student.otago.ac.nz>
2023-11-14 00:29:23 +1300

    Update autoencoder script with actual script

--- autoencoder/autoencoderScript.sh
+++ autoencoder/autoencoderScript.sh
@@ -1,10 +1,26 @@
-python constructSettings.py --epochs 10 \
-            --hiddenLayerShapes 128 64 64 \
-            --latentDimension 16 \
-            --encoderActivation sigmoid \
-            --targetClasses 0 1 \
-            --encoderLossWeighting 0.01
+EPOCHS=10
+HIDDEN_LAYER_SHAPES="128 64 64"
+ENCODER_ACTIVATION="sigmoid"
+TARGET_CLASSES="0 1"
 
-python main.py
+for trialIndex in {0..100}; do
+    for LATENT_DIMENSION in {16,64,128}; do
+        for ENCODER_LOSS_WEIGHTING in $(seq 0 0.05 0.5); do
+            printf "\33[2K\r"
+            printf "TRIAL: $trialIndex\tLATENT DIMENSION: $LATENT_DIMENSION\tENCODER LOSS WEIGHTING: $ENCODER_LOSS_WEIGHTING\r"
 
-python aggregateHistory.py
+            python constructSettings.py --epochs $EPOCHS \
+                        --hiddenLayerShapes $HIDDEN_LAYER_SHAPES \
+                        --latentDimension $LATENT_DIMENSION \
+                        --encoderActivation $ENCODER_ACTIVATION \
+                        --targetClasses $TARGET_CLASSES \
+                        --encoderLossWeighting $ENCODER_LOSS_WEIGHTING
+
+            python main.py &> main.log
+            python aggregateHistory.py &> aggregate.log
+
+        done
+    done
+done
+
+
